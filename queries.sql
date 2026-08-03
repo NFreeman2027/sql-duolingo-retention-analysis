f433b2a7eae1c6ee27302a110c8c1bbe3c273bcf
NFreeman2027 <electricgypsy72@gmail.com>
2026-08-03 16:21:28 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,72 @@
+# SQL analysis of 13M Duolingo Sessions from Kaggle.com - https://www.kaggle.com/datasets/kylefengkfeng209/how-fast-do-you-forget-13m-duolingo-sessions?select=forgetting_curve.csv
+# 3 Queries to examine the retention of people trying to learn Spanish with Duolingo Lessons
+# ------------------------------------------------------------------------------  
 # * Retention over time -
-https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sf983d2d7-8b33-4e6e-8518-189255042352!2e1
-  
+SELECT 
+    lag_bin,
+    ROUND(AVG(pct_perfect_sessions), 2) AS avg_pct_perfect
+FROM 
+    `sql-portfolio-project-504414.duolingo_data.forgettin_curve`
+WHERE 
+    language_name = 'Spanish'
+GROUP BY 
+    lag_bin, lag_bin_order
+ORDER BY 
+    lag_bin_order ASC;
+# -----------------------------------------------------------------------------
+# Key Finding: A SQL analysis of Spanish language practice intervals reveals that session accuracy remains relatively steady (~83%–86%) across the 
+# first month of inactivity, but suffers a sharp drop-off to 78.91% when practice lags by 3 months or longer.
+# -----------------------------------------------------------------------------
 # * Practice vs. Retention - 
-https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sad421893-df34-4f45-99a5-dc96ef8db2c5!2e1
-  
+SELECT 
+    practice_bin,
+    practice_bin_order,
+    SUM(n_traces) AS total_observations,
+    ROUND(AVG(pct_perfect_sessions), 2) AS avg_pct_perfect_sessions
+FROM 
+    `sql-portfolio-project-504414.duolingo_data.forgettin_curve`
+WHERE 
+    language_name = 'Spanish'
+GROUP BY 
+    practice_bin,
+    practice_bin_order
+ORDER BY 
+    practice_bin_order ASC;
+# -----------------------------------------------------------------------------
+# Key Takeaway : Analysis revealed an initial slight dip in perfection rate during early practice stages (3-4 exposures at 82.83%),
+# followed by steady recovery and plateauing near 84% accuracy once an item reaches 10+ exposures  
+# -----------------------------------------------------------------------------
 # * Top 3 Best Retention vs. Highest Forgetting - 
-https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sa67e8cc5-6e89-4e71-8673-d2bea5eb28fd!2e1
+-- Top 3 Best Performing Windows
+SELECT 
+    'Best Retention' AS category,
+    lag_bin,
+    practice_bin,
+    pct_perfect_sessions
+FROM 
+    `sql-portfolio-project-504414.duolingo_data.forgettin_curve`
+WHERE 
+    language_name = 'Spanish'
+ORDER BY 
+    pct_perfect_sessions DESC
+LIMIT 3;
+
+-- Bottom 3 Risk Windows
+SELECT 
+    'Highest Forgetting Risk' AS category,
+    lag_bin,
+    practice_bin,
+    pct_perfect_sessions
+FROM 
+    `sql-portfolio-project-504414.duolingo_data.forgettin_curve`
+WHERE 
+    language_name = 'Spanish'
+ORDER BY 
+    pct_perfect_sessions ASC
+LIMIT 3;
+# -----------------------------------------------------------------------------
+# Key Finding : Recency is most important. All top 3 are <1 hour Lag_bin
+# All 3 Highest Risk Windows are 3+ months lag_bin
+# So, product should focus on notification alerts and flash card promotion
+# over building lifetime repetition counts to drive retention and success
+
