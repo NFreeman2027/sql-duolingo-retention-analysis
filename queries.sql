500fe1c5ca53a1a1acfc1765ebbb11aec3d94eb6
NFreeman2027 <electricgypsy72@gmail.com>
2026-08-03 15:35:58 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,3 @@
-
+# * Retention over time - https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sf983d2d7-8b33-4e6e-8518-189255042352!2e1
+# * Practice vs. Retention - https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sad421893-df34-4f45-99a5-dc96ef8db2c5!2e1
+# * Top 3 Best Retention vs. Highest Forgetting - https://console.cloud.google.com/bigquery?ws=!1m7!1m6!12m5!1m3!1ssql-portfolio-project-504414!2sus-central1!3sa67e8cc5-6e89-4e71-8673-d2bea5eb28fd!2e1
